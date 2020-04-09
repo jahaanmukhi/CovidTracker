bc9293649133a4c4eed1d0b3951b148f3e59dfdb
jad111 <jad111@duke.edu>
2020-04-09 19:05:54 -0400

    Updates data when the update endpoint is hit

--- online/app.py
+++ online/app.py
@@ -1,14 +1,27 @@
 from flask import Flask
 import json
+import threading
+import update
 
 app = Flask(__name__)
 
 
 @app.route('/map')
 def get_map():
+    """
+    Grabs the map data.
+    """
     with open('data/timeseries.json', 'r') as json_file:
         return json.load(json_file)
 
+@app.route('/map/update')
+def update():
+    """
+    Forces an update of the server's data.
+    """
+    update.update()
 
 if __name__ == '__main__':
+    update_thread = threading.Thread(target=update.watch_for_updates, args=())
+    update_thread.start()
     app.run()
