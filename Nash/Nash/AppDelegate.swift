4fc197d4d9d8a3aaff0d88c4484cff7646cc9faf
Ava LeWinter <ava.lewinter@duke.edu>
2020-04-09 19:04:32 -0700

    test

--- Nash/Nash/AppDelegate.swift
+++ Nash/Nash/AppDelegate.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
