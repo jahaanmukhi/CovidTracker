95898422971a5c83b834b907eed963b31a495528
Joseph Nagy <jn135@duke.edu>
2020-04-12 21:34:08 -0400

    added Covid and Pin structs, also loop to populate map with pins

--- Nash/Nash/Emergency.swift
+++ Nash/Nash/Emergency.swift
@@ -9,24 +9,72 @@
 import Foundation
 import MapKit
 
-class Emergency: NSObject, MKAnnotation {
-  let title: String?
-  let locationName: String?
-  let coordinate: CLLocationCoordinate2D
+class Results: Codable{
+    let results: [Covid]
+    
+    init(results: [Covid]) {
+        self.results = results
+    }
+}
+
+class Covid: Codable {
+    let uid: Int?
+    let fips: Double?
+    let combined_key: String?
+    let country: String?
+    let state: String?
+    let county: String?
+    let latitude: Double!
+    let longitude: Double!
+    let confirmed_cases: Int!
+    let deaths: Int!
+    let daily_change_cases: Int?
+    let daily_change_deaths: Int?
+    let population: Int?
 
   init(
-    title: String?,
-    locationName: String?,
-    coordinate: CLLocationCoordinate2D
+    uid:Int?,
+    fips:Double?,
+    combined_key:String?,
+    country:String?,
+    state:String?,
+    county:String?,
+    latitude:Double!,
+    longitude:Double!,
+    confirmed_cases:Int?,
+    deaths:Int?,
+    daily_change_cases:Int?,
+    daily_change_deaths:Int?,
+    population:Int?
   ) {
-    self.title = title
-    self.locationName = locationName
-    self.coordinate = coordinate
-
-    super.init()
+    self.uid = uid
+    self.fips = fips
+    self.combined_key = combined_key
+    self.country = country
+    self.state = state
+    self.county = county
+    self.latitude = latitude
+    self.longitude = longitude
+    self.confirmed_cases = confirmed_cases
+    self.deaths = deaths
+    self.daily_change_cases = daily_change_cases
+    self.daily_change_deaths = daily_change_deaths
+    self.population = population
   }
+}
 
-  var subtitle: String? {
-    return locationName
-  }
+class Pin: NSObject, MKAnnotation{
+    let coordinate: CLLocationCoordinate2D
+    let title:String?
+    let subtitle:String?
+    
+    init(
+        coordinate:CLLocationCoordinate2D,
+        title:String?,
+        subtitle:String?
+    ) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
 }
--- Nash/Nash/MapViewController.swift
+++ Nash/Nash/MapViewController.swift
@@ -11,20 +11,84 @@ import MapKit
 
 class MapViewController: UIViewController {
     @IBOutlet weak var mapView: CoronavirusMapView!
+
+    let allData: [Covid] = []
+    
+    func getAllData() {
+           
+           // 2. BEGIN NETWORKING code
+           //
+                   let mySession = URLSession(configuration: URLSessionConfiguration.default)
+
+                   let url = URL(string: "https://nash-273721.df.r.appspot.com/map")!
+
+           // 3. MAKE THE HTTPS REQUEST task
+           //
+                   let task = mySession.dataTask(with: url) { data, response, error in
+
+                       // ensure there is no error for this HTTP response
+                       guard error == nil else {
+                           print ("error: \(error!)")
+                           return
+                       }
+
+                       // ensure there is data returned from this HTTP response
+                       guard let jsonData = data else {
+                           print("No data")
+                           return
+                       }
+                       
+                       print("Got the data from network")
+           // 4. DECODE THE RESULTING JSON
+           //
+                       let decoder = JSONDecoder()
+
+                       do {
+                           // decode the JSON into our array of todoItem's
+                           let allData = try decoder.decode([Covid].self, from: jsonData)
+                        
+//                        for data in allData{
+//                            print("Country is: \(data.country)")
+//                        }
+                           
+                       } catch {
+                           print("JSON Decode error")
+                       }
+                   }
+
+               // actually make the http task run.
+               task.resume()
+           
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getAllData()
         
-        // Set initial location in Honolulu
-        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        for item in allData{
+            print("Country (in for loop): \(item.country ?? "no country listed")")
+        }
+        
+        // Set initial location
+        let initialLocation = CLLocation(latitude: 39.86746056019632, longitude: -74.19296019422868)
 
         // Do any additional setup after loading the view.
         mapView.centerToLocation(initialLocation)
         
-        let artwork = Emergency(
-          title: "King David Kalakaua",
-          locationName: "Waikiki Gateway Park",
-          coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
-        mapView.addAnnotation(artwork)
+        //dummy data 
+        let Case1: Covid = Covid(uid: 1, fips: 100.1, combined_key: "key", country: "USA", state: "NJ", county: "Ocean", latitude: 39.86746056019632, longitude: -74.19296019422868, confirmed_cases: 5, deaths: 10, daily_change_cases: 0, daily_change_deaths: 0, population: 0)
+        
+        let Case2: Covid = Covid(uid: 1, fips: 100.1, combined_key: "key", country: "Canada", state: "NJ", county: "Bergen", latitude: 39.868215, longitude: -74.192864, confirmed_cases: 100, deaths: 50, daily_change_cases: 0, daily_change_deaths: 0, population: 0)
+
+        let cases: [Covid] = [Case1, Case2]
+        
+        //populate map with pins
+        for c in cases{
+            let description = "Cases: \(c.confirmed_cases ?? 0)) \n Deaths: \(c.deaths ?? 0)"
+            let location = "\(c.county ?? "No COUNTY") County, \(c.state ?? "NO STATE")"
+            let annotation = Pin(coordinate: CLLocationCoordinate2D(latitude: c.latitude, longitude: c.longitude ), title: location, subtitle: description)
+            mapView.addAnnotation(annotation)
+        }
     }
     
 
